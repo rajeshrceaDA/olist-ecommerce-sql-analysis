d06b3251da9ae51089d9a184496ca71e8e8726ef
rajeshrceaDA <rajeshrcea70@gmail.com>
2026-07-02 09:31:19 +0530

    Add Data Cleaning SQL file
    Created the first SQL module for data cleaning and validation.

---
+++ SQL/01_Data_Cleaning.sql
@@ -0,0 +1,10 @@
+/*
+=========================================
+Project : Olist E-commerce SQL Analysis
+Module  : Data Cleaning & Data Validation
+Author  : Rajesh Kumar
+Database: MySQL
+=========================================
+*/
+
+-- Data Cleaning Queries
